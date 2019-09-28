3c60288cfff56dc1f00b34bc0435451d0959bb38
Artemij <artemijdeleto@gmail.com>
2019-09-28 18:57:35 +0400

    Add support for dev & production mode

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,30 @@
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/gospy.js',
-      format: 'iife'
-    }
-  ],
-  plugins: [
-    terser()
-  ]
-};
+let config = {};
+
+if (process.env.BUILD === 'development') {
+  config = {
+    input: 'src/index.js',
+    output: [
+      {
+        file: 'dist/gospy.js',
+        format: 'iife'
+      }
+    ]
+  };
+} else {
+  config = {
+    input: 'src/index.js',
+    output: [
+      {
+        file: 'dist/gospy.min.js',
+        format: 'iife'
+      }
+    ],
+    plugins: [
+      terser()
+    ]
+  };
+}
+
+export default config;
